2180f541c3013a70ef96b08022b194e944a2185c
Andrew Ross <andrew@vermonster.com>
2016-11-25 09:52:22 -0500

    work on solving modular linear systems, but not there yet

--- modular_matrix.py
+++ modular_matrix.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import defaultdict
 
 def modmat_mult(C, D, mod):
   (m, n) = C.shape
@@ -11,18 +12,31 @@ def modmat_mult(C, D, mod):
   return E
 
 def modmat_dot(A, x, mod):
-  return modmat_mult(A, np.array([x], dtype=np.int8).T, mod)
+  return modmat_mult(A, np.array([x], dtype=np.int8).T, mod).T[0]
+
+moddiv_cache = defaultdict(lambda: defaultdict(dict))
+def mod_divide(a, b, mod):
+  if mod not in moddiv_cache:
+    for i in range(mod):
+      for j in range(mod):
+        moddiv_cache[mod][(i * j) % mod][i] = j
+        moddiv_cache[mod][(i * j) % mod][j] = i
+  return moddiv_cache[mod][a][b]
+
+assert(mod_divide(1, 2, 3) == 2)
+assert(mod_divide(2, 1, 3) == 2)
 
 def modmat_fsolve(L, b, mod):
   if not np.all(np.diag(L)):
     raise ValueError("Matrix is singular")
   n = L.shape[0]
   x = np.empty(n, dtype=np.int8)
-  for i in range(n):
-    s = b[i]
-    for j in range(0, i):
-      s = (s + L[i, j] * x[j]) % mod
-    x[i] = s
+  for j in range(n):
+    x[j] = b[j]
+    for k in range(j):
+      x[j] -= L[j, k] * x[k]
+    x[j] = x[j] % mod
+    x[j] = mod_divide(x[j], L[j, j], mod)
   return x
 
 def modmat_rsolve(U, b, mod):
@@ -30,20 +44,21 @@ def modmat_rsolve(U, b, mod):
     raise ValueError("Matrix is singular")
   n = U.shape[0]
   x = np.empty(n, dtype=np.int8)
-  for i in range(n - 1, -1, -1):
-    s = b[i]
-    for j in range(i + 1, n):
-      s = (s + U[i, j] * x[j]) % mod
-    x[i] = s
+  for j in reversed(range(n)):
+    x[j] = b[j]
+    for k in range(j+1, n):
+      x[j] -= U[j, k] * x[k]
+    x[j] = x[j] % mod
+    x[j] = mod_divide(x[j], U[j, j], mod)
   return x
 
 def modmat_lu(A, mod):
-  n = A.shape[0]
+  (m,n) = A.shape
+  if m != n: raise ValueError("must pass a square matrix")
   U = np.copy(A)
   L = np.identity(n, dtype=np.int8)
   P = np.identity(n, dtype=np.int8)
-  for j in range(0, n - 1):
-    #select row with a 1
+  for j in range(n-1):
     for i in range(j, n):
       if U[i, j] == 1:
         #swap rows of U
@@ -59,11 +74,11 @@ def modmat_lu(A, mod):
         P[j, :] = P[i, :]
         P[i, :] = temp
         break
-    for i in range(j + 1, n):
-      L[i, j] = U[i, j]
-      #eliminate column
-      for k in range(j, n):
-        U[i, k] = (U[i, k] + L[i, j] * U[j, k]) % mod
+  # Now apply the normal LU operations
+  for i in range(j+1, n):
+    L[i,j] = mod_divide(U[i,j], U[j,j], mod)
+    for k in range(j, n):
+      U[i,k] = (U[i,k] - (L[i,j] * U[j,k])) % mod
 
   return P, L, U
 
@@ -79,6 +94,15 @@ def modmat_rank(A, mod):
 
 def modmat_nullity(A, mod):
   return A.shape[0] - modmat_rank(A, mod)
+
+def transition_matrix(adj_fn, rows, cols):
+  A = np.zeros((rows*cols, rows*cols), dtype=np.int8)
+  for row in range(rows):
+    for col in range(cols):
+      for i, j, k in adj_fn(row, col):
+        if 0 <= i < rows and 0 <= j < cols:
+          A[row+col*rows][i+j*rows] = k
+  return A
 
 if __name__ == '__main__':
   print('running tests...')
@@ -146,6 +170,23 @@ if __name__ == '__main__':
                 [0, 2, 0]], dtype=np.int8)
   x = np.array([1, 2, 0], dtype=np.int8)
   b = np.array([2, 2, 1], dtype=np.int8)
-  np.testing.assert_array_equal(modmat_dot(A, x, 3), np.array([b]).T)
+  np.testing.assert_array_equal(modmat_dot(A, x, 3), b)
+
+  transition_fn = lambda i, j: [[i,j,2],[i+1,j,1],[i-1,j,1],[i,j+1,1],[i,j-1,1]]
+  A = transition_matrix(transition_fn, 3, 3)
+  grid = np.array([[0,1,1],[1,0,0],[0,1,1]])
+  presses = modmat_solve(A, grid.ravel(), 3)
+  print(grid)
+  print(modmat_dot(A, presses, 3).reshape(grid.shape))
+
+  # state = grid.ravel()
+  # print(state.reshape(grid.shape))
+  # for i, n in enumerate(presses):
+    # for _ in range(n):
+      # press = np.zeros(len(presses), dtype=np.int8)
+      # press[i] = 1
+      # effect = modmat_dot(A, press, 3)
+      # state = (state + effect) % 3
+      # print(state.reshape(grid.shape))
 
   print('done!')
