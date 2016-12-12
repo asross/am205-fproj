82e61646c04e84c8cca173aa5493dc246036cf30
Andrew Ross <andrew@vermonster.com>
2016-12-12 11:35:03 -0500

    more nullities

--- calculate_nullities.py
+++ calculate_nullities.py
@@ -5,7 +5,7 @@ import numpy as np
 if __name__ == '__main__':
   adj = lambda i, j: [[i,j,1],[i+1,j,1],[i-1,j,1],[i,j+1,1],[i,j-1,1]]
   print('mod,n,m,nullity')
-  for mod in range(2, 6):
+  for mod in range(2, 18):
     for n in range(1, 17):
       for m in range(1, 17):
         A = transition_matrix(adj, n, m, mod)
